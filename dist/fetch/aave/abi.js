3336d170ccb0ee26490f16bc5c2e37c64ce02117
Achthar <69119535+Achthar@users.noreply.github.com>
2025-09-12 00:12:03 +0200

    more pools

--- dist/fetch/aave/abi.js
+++ dist/fetch/aave/abi.js
@@ -1,104 +1,124 @@
 export const AAVE_ABIS = (noSToken) => [
     {
-        "inputs": [],
-        "name": "getPriceOracle",
-        "outputs": [{
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getAddressesProvider",
-        "outputs": [{
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getPoolDataProvider",
-        "outputs": [{
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "POOL",
-        "outputs": [{
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "ADDRESSES_PROVIDER",
-        "outputs": [{
-                "internalType": "contract IPoolAddressesProvider",
-                "name": "",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getReservesList",
-        "outputs": [{
-                "internalType": "address[]",
-                "name": "",
-                "type": "address[]"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [{
-                "internalType": "address",
-                "name": "asset",
-                "type": "address"
-            }],
-        "name": "getReserveTokensAddresses",
-        "outputs": noSToken ? [{
-                "internalType": "address",
-                "name": "aTokenAddress",
-                "type": "address"
-            },
+        inputs: [],
+        name: "getPriceOracle",
+        outputs: [
             {
-                "internalType": "address",
-                "name": "variableDebtTokenAddress",
-                "type": "address"
-            }] : [{
-                "internalType": "address",
-                "name": "aTokenAddress",
-                "type": "address"
+                internalType: "address",
+                name: "",
+                type: "address",
             },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "getAddressesProvider",
+        outputs: [
             {
-                "internalType": "address",
-                "name": "stableDebtTokenAddress",
-                "type": "address"
+                internalType: "address",
+                name: "",
+                type: "address",
             },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "getPoolDataProvider",
+        outputs: [
             {
-                "internalType": "address",
-                "name": "variableDebtTokenAddress",
-                "type": "address"
-            }],
-        "stateMutability": "view",
-        "type": "function"
-    }
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "POOL",
+        outputs: [
+            {
+                internalType: "address",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "ADDRESSES_PROVIDER",
+        outputs: [
+            {
+                internalType: "contract IPoolAddressesProvider",
+                name: "",
+                type: "address",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [],
+        name: "getReservesList",
+        outputs: [
+            {
+                internalType: "address[]",
+                name: "",
+                type: "address[]",
+            },
+        ],
+        stateMutability: "view",
+        type: "function",
+    },
+    {
+        inputs: [
+            {
+                internalType: "address",
+                name: "asset",
+                type: "address",
+            },
+        ],
+        name: "getReserveTokensAddresses",
+        outputs: noSToken
+            ? [
+                {
+                    internalType: "address",
+                    name: "aTokenAddress",
+                    type: "address",
+                },
+                {
+                    internalType: "address",
+                    name: "variableDebtTokenAddress",
+                    type: "address",
+                },
+            ]
+            : [
+                {
+                    internalType: "address",
+                    name: "aTokenAddress",
+                    type: "address",
+                },
+                {
+                    internalType: "address",
+                    name: "stableDebtTokenAddress",
+                    type: "address",
+                },
+                {
+                    internalType: "address",
+                    name: "variableDebtTokenAddress",
+                    type: "address",
+                },
+            ],
+        stateMutability: "view",
+        type: "function",
+    },
 ];
 export var AaveFetchFunctions;
 (function (AaveFetchFunctions) {
--- src/fetch/aave/abi.ts
+++ src/fetch/aave/abi.ts
@@ -1,111 +1,130 @@
-
 export const AAVE_ABIS = (noSToken: boolean) => [
-    {
-        "inputs": [],
-        "name": "getPriceOracle",
-        "outputs": [{
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getAddressesProvider",
-        "outputs": [{
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getPoolDataProvider",
-        "outputs": [{
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "POOL",
-        "outputs": [{
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "ADDRESSES_PROVIDER",
-        "outputs": [{
-            "internalType": "contract IPoolAddressesProvider",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
+  {
+    inputs: [],
+    name: "getPriceOracle",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAddressesProvider",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getPoolDataProvider",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "POOL",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "ADDRESSES_PROVIDER",
+    outputs: [
+      {
+        internalType: "contract IPoolAddressesProvider",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 
-    {
-        "inputs": [],
-        "name": "getReservesList",
-        "outputs": [{
-            "internalType": "address[]",
-            "name": "",
-            "type": "address[]"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [{
-            "internalType": "address",
-            "name": "asset",
-            "type": "address"
-        }],
-        "name": "getReserveTokensAddresses",
-        "outputs": noSToken ? [{
-            "internalType": "address",
-            "name": "aTokenAddress",
-            "type": "address"
-        },
-        {
-            "internalType": "address",
-            "name": "variableDebtTokenAddress",
-            "type": "address"
-        }] : [{
-            "internalType": "address",
-            "name": "aTokenAddress",
-            "type": "address"
-        },
-        {
-            "internalType": "address",
-            "name": "stableDebtTokenAddress",
-            "type": "address"
-        },
-        {
-            "internalType": "address",
-            "name": "variableDebtTokenAddress",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    }
-]
+  {
+    inputs: [],
+    name: "getReservesList",
+    outputs: [
+      {
+        internalType: "address[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "asset",
+        type: "address",
+      },
+    ],
+    name: "getReserveTokensAddresses",
+    outputs: noSToken
+      ? [
+          {
+            internalType: "address",
+            name: "aTokenAddress",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "variableDebtTokenAddress",
+            type: "address",
+          },
+        ]
+      : [
+          {
+            internalType: "address",
+            name: "aTokenAddress",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "stableDebtTokenAddress",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "variableDebtTokenAddress",
+            type: "address",
+          },
+        ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
 export enum AaveFetchFunctions {
-    getReservesList = "getReservesList",
-    ADDRESSES_PROVIDER = "ADDRESSES_PROVIDER",
-    getPriceOracle = "getPriceOracle",
-    getReserveTokensAddresses = "getReserveTokensAddresses"
+  getReservesList = "getReservesList",
+  ADDRESSES_PROVIDER = "ADDRESSES_PROVIDER",
+  getPriceOracle = "getPriceOracle",
+  getReserveTokensAddresses = "getReserveTokensAddresses",
 }
