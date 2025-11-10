a541753b54a07c3c7a68aee73f161452bb5ae50d
Achthar <69119535+Achthar@users.noreply.github.com>
2025-11-10 11:00:50 +0100

    minors

--- dist/fetch/aave/fetchOracles.js
+++ dist/fetch/aave/fetchOracles.js
@@ -2,6 +2,7 @@ import { getEvmClientWithCustomRpcs } from "@1delta/providers";
 import { AAVE_ABIS, AaveFetchFunctions } from "./abi.js";
 import { sleep } from "../../utils.js";
 import { Lender } from "@1delta/lender-registry";
+import { Chain } from "@1delta/chain-registry";
 // aproach for aave
 // get reserve list from pool
 // fetch tokens per reserve from address provider
@@ -10,12 +11,15 @@ export async function fetchAaveTypePriceOracles(AAVE_FORK_POOL_DATA) {
     const forks = Object.keys(AAVE_FORK_POOL_DATA);
     let forkMap = {};
     for (const fork of forks) {
-        if (fork === Lender.KLAYBANK)
-            continue;
         // @ts-ignore
         const addressSet = AAVE_FORK_POOL_DATA[fork];
         const chains = Object.keys(addressSet);
         let dataMap = {};
+        if (fork === Lender.KLAYBANK) {
+            dataMap[Chain.KAIA_MAINNET] =
+                "0xa4BCd83C6d6C75ED9E029cde2DD24bAc2f3C5B59";
+            continue;
+        }
         for (const chain of chains) {
             const client = getEvmClientWithCustomRpcs(chain);
             const addresses = addressSet[chain];
