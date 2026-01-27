4fabf568d6db2748f7f80e1674b0d65509f1ed64
Achthar <69119535+Achthar@users.noreply.github.com>
2026-01-27 11:43:11 +0100

    fix:  add comet oracles

--- src/fetch/compound-v3/abi.ts
+++ src/fetch/compound-v3/abi.ts
@@ -1,94 +1,112 @@
-
 export const COMET_ABIS = [
-    {
-        "inputs": [],
-        "name": "baseToken",
-        "outputs": [{
-            "internalType": "address",
-            "name": "",
-            "type": "address"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "numAssets",
-        "outputs": [{
-            "internalType": "uint8",
-            "name": "",
-            "type": "uint8"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "baseBorrowMin",
-        "outputs": [{
-            "internalType": "uint256",
-            "name": "",
-            "type": "uint256"
-        }],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [{
-            "internalType": "uint8",
-            "name": "i",
-            "type": "uint8"
-        }],
-        "name": "getAssetInfo",
-        "outputs": [{
-            "components": [{
-                "internalType": "uint8",
-                "name": "offset",
-                "type": "uint8"
-            },
-            {
-                "internalType": "address",
-                "name": "asset",
-                "type": "address"
-            }, {
-                "internalType": "address",
-                "name": "priceFeed",
-                "type": "address"
-            },
-            {
-                "internalType": "uint64",
-                "name": "scale",
-                "type": "uint64"
-            },
-            {
-                "internalType": "uint64",
-                "name": "borrowCollateralFactor",
-                "type": "uint64"
-            },
-            {
-                "internalType": "uint64",
-                "name": "liquidateCollateralFactor",
-                "type": "uint64"
-            },
-            {
-                "internalType": "uint64",
-                "name": "liquidationFactor",
-                "type": "uint64"
-            },
-            {
-                "internalType": "uint128",
-                "name": "supplyCap",
-                "type": "uint128"
-            }],
-            "internalType": "struct CometCore.AssetInfo",
-            "name": "", "type": "tuple"
-        }], "stateMutability": "view", "type": "function"
-    }
-]
+  {
+    inputs: [],
+    name: "baseToken",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAssets",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "baseBorrowMin",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "i",
+        type: "uint8",
+      },
+    ],
+    name: "getAssetInfo",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint8",
+            name: "offset",
+            type: "uint8",
+          },
+          {
+            internalType: "address",
+            name: "asset",
+            type: "address",
+          },
+          {
+            internalType: "address",
+            name: "priceFeed",
+            type: "address",
+          },
+          {
+            internalType: "uint64",
+            name: "scale",
+            type: "uint64",
+          },
+          {
+            internalType: "uint64",
+            name: "borrowCollateralFactor",
+            type: "uint64",
+          },
+          {
+            internalType: "uint64",
+            name: "liquidateCollateralFactor",
+            type: "uint64",
+          },
+          {
+            internalType: "uint64",
+            name: "liquidationFactor",
+            type: "uint64",
+          },
+          {
+            internalType: "uint128",
+            name: "supplyCap",
+            type: "uint128",
+          },
+        ],
+        internalType: "struct CometCore.AssetInfo",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  { internalType: "address", name: "baseToken", type: "address" },
+  { internalType: "address", name: "baseTokenPriceFeed", type: "address" },
+];
 
 export enum CompoundV3FetchFunctions {
-    baseToken = "baseToken",
-    getAssetInfo = "getAssetInfo",
-    numAssets = "numAssets",
-    baseBorrowMin = "baseBorrowMin",
+  baseToken = "baseToken",
+  getAssetInfo = "getAssetInfo",
+  numAssets = "numAssets",
+  baseBorrowMin = "baseBorrowMin",
+  baseTokenPriceFeed = "baseTokenPriceFeed",
 }
--- src/fetch/compound-v3/fetchEverything.ts
+++ src/fetch/compound-v3/fetchEverything.ts
@@ -7,12 +7,20 @@ import { multicallRetry, readJsonFile } from "../utils/index.js";
 
 type CompoundV3Map = {
   [cometId: string]: {
-    [chainid: string | number]: {
+    [chainid: string]: {
       baseAsset: string;
       baseBorrowMin: bigint;
       baseAsetSymbol: string;
       reserves: string[];
       nAssets: number;
+    };
+  };
+};
+
+type AddressChainMap = {
+  [cometId: string]: {
+    [chainid: string]: {
+      [asset: string]: string;
     };
   };
 };
@@ -29,16 +37,17 @@ export async function fetchCompoundV3Data(): Promise<{
   COMETS_PER_CHAIN_MAP: any;
 }> {
   let cometDataMap: CompoundV3Map = {};
+  let cometOracles: AddressChainMap = {};
   let compoundReserves: any = {};
   let compoundBaseData: any = {};
   const COMETS_PER_CHAIN_MAP = await readJsonFile(
-    "./config/compound-v3-pools.json"
+    "./config/compound-v3-pools.json",
   );
   const chains = Object.keys(COMETS_PER_CHAIN_MAP);
   for (const chain of chains) {
     const comets = Object.values(COMETS_PER_CHAIN_MAP[chain]);
 
-    const CometMetas = (await multicallRetry({
+    const cometMetas = (await multicallRetry({
       chainId: chain,
       allowFailure: false,
       contracts: comets
@@ -61,6 +70,12 @@ export async function fetchCompoundV3Data(): Promise<{
             address: comet,
             args: [],
           },
+          {
+            abi: COMET_ABIS,
+            functionName: CompoundV3FetchFunctions.baseTokenPriceFeed,
+            address: comet,
+            args: [],
+          },
         ])
         .flat() as any[],
     })) as any;
@@ -68,10 +83,8 @@ export async function fetchCompoundV3Data(): Promise<{
     const cometKeys = Object.keys(COMETS_PER_CHAIN_MAP[chain]);
     for (let i = 0; i < comets.length; i++) {
       const comet = comets[i];
-      const [numAssetsesult, baseAssetResult, baseBorrowMin] = CometMetas.slice(
-        3 * i,
-        3 * i + 3
-      );
+      const [numAssetsesult, baseAssetResult, baseBorrowMin, baseTokenFeed] =
+        cometMetas.slice(3 * i, 3 * i + 3);
       const nAssets = numAssetsesult;
       const baseAsset = baseAssetResult.toLowerCase();
       const cometIndexes = Array.from({ length: nAssets }, (_, i) => i);
@@ -87,14 +100,26 @@ export async function fetchCompoundV3Data(): Promise<{
       })) as any;
 
       const underlyings = cometIndexes.map((i) =>
-        underlyingDatas[i].asset.toLowerCase()
+        underlyingDatas[i].asset.toLowerCase(),
       );
+
       if (!cometDataMap[cometKeys[i]]) cometDataMap[cometKeys[i]] = {};
+      if (!cometOracles[cometKeys[i]]) cometOracles[cometKeys[i]] = {};
+
+      if (!cometOracles[cometKeys[i]][chain])
+        cometOracles[cometKeys[i]][chain] = {};
+
       if (!compoundBaseData[cometKeys[i]]) compoundBaseData[cometKeys[i]] = {};
       if (!compoundReserves[cometKeys[i]]) compoundReserves[cometKeys[i]] = {};
 
+      underlyings.forEach((a, j) => {
+        cometOracles[cometKeys[i]][chain][a] = underlyingDatas[j].priceFeed;
+      });
+
+      cometOracles[cometKeys[i]][chain][baseAsset] = baseTokenFeed;
+
       compoundReserves[cometKeys[i]][chain] = [baseAsset, ...underlyings].map(
-        (r) => r.toLowerCase()
+        (r) => r.toLowerCase(),
       );
       compoundBaseData[cometKeys[i]][chain] = { baseAsset, baseBorrowMin };
       cometDataMap[cometKeys[i]][chain] = {
